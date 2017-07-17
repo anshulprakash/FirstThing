f23d184f1d5dbb5fc5ea01255ac1ee08d26a1eb9
Anshul Prakash <anshul.prakash17@gmail.com>
2017-07-17 10:26:14 -0500

    fetching list of tasks

--- functions/index.js
+++ functions/index.js
@@ -16,8 +16,8 @@ exports.firstThing = functions.https.onRequest((request, response) => {
 
 function readList (app) {
 	let userId = app.getUser().userId;
-	let givenName = app.getArgument('given-name');
-	let listName = app.getArgument('list_name');
+	let givenName = app.getArgument('given-name').toLowerCase();
+	let listName = app.getArgument('list_name').toLowerCase();
 	console.log(userId);
 	console.log(givenName);
 	console.log(listName);
@@ -31,8 +31,40 @@ function readList (app) {
 	noderequest(options)  
 	  .then(function (response) { 
 	  	console.log('response from database: '+ JSON.stringify(response));
-	    app.ask(app.buildRichResponse()
-              .addSimpleResponse(JSON.stringify(response)));
+
+	  	let people = Object.keys(response);
+
+	  	if(people.indexOf(givenName) == -1){
+	  		//givenName does not have any list
+	  	}
+	  	console.log(people)
+	  	let lists = Object.keys(response[givenName]);
+
+	  	if(lists.indexOf(listName) == -1){
+	  		//givenName does not have any list by the name listName
+	  	}
+
+	  	let tasks = Object.keys(response[givenName][listName]);
+	  	let taskArray = [];
+
+		tasks.forEach(function(task){
+			let value = response[givenName][listName][task]['task'];
+			taskArray.push(value);
+		});
+
+		const taskList = app.buildList('List of tasks');
+		let count = 1;
+		taskArray.forEach(function(task){
+			taskList.addItems(app.buildOptionItem(task,['1', 'First one'])
+            .setTitle(task)
+            .setDescription(task))
+		});
+		console.log(taskList);
+
+	    app.askWithList(app.buildRichResponse()
+			.addSimpleResponse("Alright. Here are your tasks"),
+			taskList
+			);
     	return;
 	  })
 	  .catch(function (err) {
